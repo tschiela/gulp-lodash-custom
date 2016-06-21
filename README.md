b18aecbdf18e9ff7043c91bc712e1172ca4b4a3f
Thomas Schiela <thomas.schiela@avaloq.com>
2016-06-21 14:33:37 +0200

    Improved readme, added package.json, added .gitignore

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
 # gulp-lodash-custom
 gulp tasks that search for used lodash methods in codebase to do a custom build of lodash
+
+## Install
+
+```shell
+$ npm install gulp-lodash-custom --save-dev
+```
+
+## Usage
+
+```javascript
+var concat = require('gulp-concat');
+var customLodash = require('gulp-lodash-custom');
+var path = require('path');
+var runSequence = require('run-sequence');
+
+var config = {
+    src: path.join(__dirname, 'src'),
+    dist: path.join(__dirname, 'dist')
+};
+
+gulp.task('lodash-custom', function () {
+    return gulp.src(path.join(config.src, 'js', '**', '*.js'), { buffer: false })
+        .pipe(customLodash({target: path.join(config.src, 'tmp', 'lodash-custom.js')}));
+});
+
+gulp.task('concatApp', function () {
+    return gulp.src(config.src, 'js', '**', '*.js')
+        .pipe(concat('app.js'))
+        .pipe(gulp.dest(config.dist));
+});
+
+// because of asynchronous tasks in gulp we need to make the concat taks wait for the lodash custom build task
+gulp.task('build', function (callback) {
+    runSequence('lodash-custom', 'concatApp', callback);
+});
+```
+
+## Options
+### forceMethods
+* `Array`
+* force to include lodash methods, event if they are not found in the code base
+* Default: `[]`
+
+### target
+* `String`
+* filepath of generated lodash custom build
+* Default: `lodash-custom.min.js`
+
+## TODO
+* Testing
---
+++ gulp-lodash-custom.js
@@ -0,0 +1,82 @@
+var _ = require('lodash');
+var exec = require('child_process').exec;
+var gutil = require('gulp-util');
+var path = require('path');
+var PluginError = gutil.PluginError;
+var through = require('through2');
+var usedLodashMethods = {};
+
+var PLUGIN_NAME = 'gulp-lodash-custom';
+
+function isLodashMethod(methodName) {
+    return typeof _[methodName] === 'function';
+};
+
+
+module.exports = function (options) {
+    var defaults = {
+        forceMethods: [],
+        target: 'lodash-custom.min.js'
+    };
+
+    if (!options.lodashCli) {
+        var lodashBin = require('lodash-cli/package.json').bin.lodash;
+        defaults.lodashCli = require.resolve('lodash-cli/' + lodashBin);
+    }
+
+    var settings = _.extend({}, defaults, options);
+    var lodashRegEx = new RegExp(/_\.(\w*)\(/g);
+
+    if (settings && settings.forceMethods) {
+        settings.forceMethods.forEach(function (method) {
+            if (isLodashMethod(method)) {
+                usedLodashMethods[method] = 1;
+            }
+        });
+    }
+
+    return through.obj(function (file, encoding, callback) {
+        if (file.isBuffer()) {
+            this.emit('error', new PluginError(PLUGIN_NAME, 'Buffers not supported!'));
+            return callback();
+        } else if (!file.isStream()) {
+            this.emit('error', new PluginError(PLUGIN_NAME, 'Only Streams are supported!'));
+            return callback();
+        } else {
+            var content = file.contents.toString();
+            var matches = lodashRegEx.exec(content);
+
+            if (matches && matches.length === 2) {
+                var method = matches[1];
+
+                if (isLodashMethod(method)) {
+                    usedLodashMethods[method] = 1;
+                }
+            }
+
+            this.push(file);
+            
+            callback();
+        }
+    }, function (callback) {
+        var methods = _.keys(usedLodashMethods).join(',');
+        var command = 'node ' + settings.lodashCli + ' include=' + methods + ' -p -o ' + settings.target;
+        var execLodashCli = exec(command);
+
+        gutil.log('starting lodash csutom build with following methods: ' + methods);
+
+        execLodashCli.stdout.on('data', function (data) {
+            gutil.log(data);
+        });
+
+        execLodashCli.stderr.on('data', function (error) {
+            callback(error);
+            return;
+        });
+
+        execLodashCli.stdout.on('end', function () {
+            callback();
+            return;
+        });
+    })
+};
